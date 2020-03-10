04dc0c8e4b001dd53a6854ff237b6eb24f303b63
Ervin Pepic <ervin.hack@gmail.com>
2020-03-10 18:39:14 +0100

    Added comment system, added products and other details

--- index.php
+++ index.php
@@ -13,20 +13,20 @@ include './footers/footer.php';
             ?>
             <?php
 
-            $sql = "SELECT id, korisnik, mesto, grad FROM testni_podaci";
+            $sql = "SELECT id, product_title, short_product_description, product_image FROM products";
             $result = mysqli_query($conn, $sql);
 
             if (mysqli_num_rows($result) > 0) {
                 //output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
             ?>
-                    <div class="col-md-4 mb-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                    <div class="col-md-4">
+                    <div class="card mt-3" style="width: 18rem;">
+                        <img src="<?php echo $row["product_image"]?>" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title"><?php echo $row["korisnik"] ?></h5>
-                            <p class="card-text"><?php echo $row["mesto"] ?></p>
-                            <a href="#" class="btn btn-primary"><?php echo $row["grad"] ?></a>
+                            <h5 class="card-title"><?php echo $row["product_title"] ?></h5>
+                            <p class="card-text"><?php echo $row["short_product_description"] ?></p>
+                            <a href="product_detail.php?id=<?php echo $row["id"]; ?>" class="btn btn-primary"><?php echo "View product" ?></a>
                         </div>
                     </div>
                     </div>
